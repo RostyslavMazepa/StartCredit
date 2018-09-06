3a741b214e05a55e7e0c302e73f19acd291d9a12
RostyslavMazepa <rostyslav.mazepa@gmail.com>
2018-09-06 14:41:21 +0300

    removed gitignored files

--- src/main/resources/db/migration/V1_1__insert_document_type.sql
+++
@@ -1,7 +0,0 @@
-------------------------------------------------------
--- document_type
-------------------------------------------------------
-INSERT INTO public.document_type (id, enable, name)
-VALUES (DEFAULT, true, 'Паспорт');
-INSERT INTO public.document_type (id, enable, name)
-VALUES (DEFAULT, true, 'ІПН');
--- src/main/resources/db/migration/V1_2__insert_phone_type.sql
+++
@@ -1,9 +0,0 @@
-------------------------------------------------------
--- phone_type
-------------------------------------------------------
-INSERT INTO public.phone_type (id, enable, name)
-VALUES (DEFAULT, true, 'Домашній');
-INSERT INTO public.phone_type (id, enable, name)
-VALUES (DEFAULT, true, 'Робочий');
-INSERT INTO public.phone_type (id, enable, name)
-VALUES (DEFAULT, true, 'Мобільний');
